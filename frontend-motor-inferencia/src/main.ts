b27f78beaee0b776666e5cb177d499271c4a7c61
Nicolás Olmos <yohannicolas2003@gmail.com>
2023-10-18 22:54:07 -0500

    Taking user id value from input and add func to handle the step by step form

--- frontend-motor-inferencia/src/main.ts
+++ frontend-motor-inferencia/src/main.ts
@@ -1,44 +1,40 @@
+import { QuestionRequestDto } from './dtos/cesarea.dto';
 const slidePage = document.querySelector(".slide-page") as HTMLDivElement;
 const nextBtnFirst: HTMLButtonElement = document.querySelector(
   ".firstNext"
 ) as HTMLButtonElement;
-const prevBtnSec = document.querySelector(".prev-1") as HTMLButtonElement;
 const nextBtnSec = document.querySelector(".next-1") as HTMLButtonElement;
-const prevBtnThird = document.querySelector(".prev-2") as HTMLButtonElement;
 const nextBtnThird = document.querySelector(".next-2") as HTMLButtonElement;
-const prevBtnFourth = document.querySelector(".prev-3") as HTMLButtonElement;
 const submitBtn = document.querySelector(".submit") as HTMLButtonElement;
 const progressText = document.querySelectorAll(".step p");
 const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
+
+const idTxt = document.getElementById("idTxt") as HTMLInputElement;
+
 let current = 1;
 
-nextBtnFirst.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
+const questionRequestDto: QuestionRequestDto = {
+  pacientId: "",
+  id: 0,
+  answer: "",
+};
+
+nextBtnFirst.addEventListener("click", (event) => {
+  // Validar que el campo de texto no esté vacío
+  if (idTxt.value === "" || idTxt.value.length < 8) {
+    alert("El número de identificación no es válido");
+    return;
+  }
+
+  // Obtener el valor del campo de texto y asignarlo al DTO
+  questionRequestDto.pacientId = idTxt.value.trim();
+
+  handleNextStep(event, 1, 25);
 });
 
-nextBtnSec.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-
-nextBtnThird.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
+nextBtnSec.addEventListener("click", (event) => handleNextStep(event, 1, 50));
+nextBtnThird.addEventListener("click", (event) => handleNextStep(event, 1, 75));
 
 submitBtn.addEventListener("click", function () {
   bullet[current - 1].classList.add("active");
@@ -51,29 +47,14 @@ submitBtn.addEventListener("click", function () {
   }, 800);
 });
 
-prevBtnSec.addEventListener("click", function (event) {
+function handleNextStep(event: Event, stepIncrement: number, slideIncrement: number) {
   event.preventDefault();
-  slidePage.style.marginLeft = "0%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
-
-prevBtnThird.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
-
-prevBtnFourth.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
+  // Avanzar al siguiente paso del formulario
+  slidePage.style.marginLeft = `-${slideIncrement}%`;
+  // Marcar el paso actual como completo
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  // Incrementar el paso actual
+  current += stepIncrement;
+}
